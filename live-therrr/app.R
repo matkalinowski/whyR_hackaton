f7f7960bc5c712735d572dec23036cc25eca006d
Patrik Drhlík <patrioscraper@gmail.com>
2019-09-26 12:29:47 +0200

    Create a base shiny app

---
+++ live-therrr/app.R
@@ -0,0 +1,28 @@
+library(shiny)
+library(shinydashboard)
+library(dplyr)
+library(tidyr)
+library(leaflet)
+
+ui <- dashboardPage(
+    dashboardHeader(title = "Live theRRR!"),
+    dashboardSidebar(),
+    dashboardBody(
+        tags$head(
+            tags$link(rel = "stylesheet", type = "text/css",
+                      href = paste0("main.css?", as.numeric(Sys.time())))
+        ),
+        leafletOutput("map")
+    )
+)
+
+server <- function(input, output) {
+    
+    output$map <- renderLeaflet({
+        leaflet() %>%
+            setView(21.0122, 52.2297, 11) %>%
+            addProviderTiles(provider = providers$OpenStreetMap)
+    })
+}
+
+shinyApp(ui, server)
