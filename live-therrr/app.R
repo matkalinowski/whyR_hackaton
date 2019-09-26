a64a418ee9551b164c357a490f9473f1d267b34e
vrata <vratislav.zabka@tul.cz>
2019-09-26 12:35:21 +0200

    Add a scalebar

--- live-therrr/app.R
+++ live-therrr/app.R
@@ -20,6 +20,7 @@ server <- function(input, output) {
     
     output$map <- renderLeaflet({
         leaflet() %>%
+            leaflet::addScaleBar('bottomleft') %>%
             setView(21.0122, 52.2297, 11) %>%
             addProviderTiles(provider = providers$OpenStreetMap)
     })
