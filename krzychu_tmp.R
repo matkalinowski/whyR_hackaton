fbda2c5a49cbfdae026522bd5cd7611160ebdd63
Krzysztof Jędrzejewski <kjedrz@gmail.com>
2019-09-26 16:24:12 +0200

    score calculations

--- krzychu_tmp.R
+++ krzychu_tmp.R
@@ -12,23 +12,84 @@ grid100m_category_distances %>%
 
 
 
+profiles = read_csv("app_data/profiles.csv")
+
+profiles = profiles %>%
+  rename(
+    category = type,
+    weight = desired
+  )
 
 criteria = tibble(
   category = c("bakery", "gym", "school"),
   weight = c(1, 0, -1)
 )
 
-score = calc_criteria_score_linear(grid100m_category_distances, criteria)
-score = calc_criteria_score_hyperbolic(grid100m_category_distances, criteria)
+criteria = tibble(
+  category = c("bakery", "gym", "school"),
+  weight = c(1, 0, -1)
+)
+
+criteria_score = calc_criteria_score_linear(grid100m_category_distances, criteria)
+criteria_score = calc_criteria_score_hyperbolic(grid100m_category_distances, criteria)
+
+ggplot(criteria_score) +
+  geom_point(aes(x, y, colour = criteria_score_norm))
+
+
+
+criteria_score = calc_criteria_score_hyperbolic(
+  grid100m_category_distances,
+  profiles %>%
+    filter(profile == 'young_parent')
+)
+
+ggplot(criteria_score) +
+  geom_point(aes(x, y, colour = criteria_score_norm))
+
+
+
+
+
+
+
+important_points = tibble(
+  lng = c(21.1, 21.01),
+  lat = c(52.1, 52.2)
+)
+
+important_points = tibble(
+  lng = c(21.11),
+  lat = c(52.15)
+)
+
+points_score = calc_points_score_linear(warsaw_100m, important_points)
+points_score = calc_points_score_hyperbolic(warsaw_100m, important_points)
+
+
+ggplot(points_score) +
+  geom_point(aes(x, y, colour = points_score_norm))
+
+
+
+
+criteria = profiles %>%
+  filter(profile == 'young_parent')
+
+important_points = tibble(
+  lng = c(21.11),
+  lat = c(52.15)
+)
+
+
+score = calc_score_linear(grid100m_category_distances, warsaw_100m, criteria, tibble())
+score = calc_score_linear(grid100m_category_distances, warsaw_100m, criteria, important_points)
+score = calc_score_hyperbolic(grid100m_category_distances, warsaw_100m, criteria, tibble())
+score = calc_score_hyperbolic(grid100m_category_distances, warsaw_100m, criteria, important_points)
+
 
 ggplot(score) +
-  geom_point(aes(x, y, colour = criteria_score))
-
-
-
-
-
-
+  geom_point(aes(x, y, colour = score_norm))
 
 
 
